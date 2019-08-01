ee33be215b5b1856f1ee194719a99ab1209354d0
Ivan Avila <ivelaval@gmail.com>
2019-08-01 16:49:07 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import './App.css';
+
+function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const [value, setValue] = useState("");
+
+  const onValueChange = ({ target: { value } }) => {
+    setValue(value);
+  };
+
+  const addTodo = () => {
+    if (value !== "") {
+      setTodos([...todos, { name: value, status: false, id: Date.now() + Math.random }]);
+      setValue("");
+    }
+  };
+
+  const handleKeyPress = ({ key }) => {
+    if (key === "Enter") {
+      addTodo();
+    }
+  };
+
+  const handleCheckboxChange = id => {
+    setTodos(todos.map( todo => {
+      if (todo.id === id) {
+        return { ...todo, status: !todo.status };
+      }
+      return todo;
+    }))
+  }
+
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  return (
+    <div className="container">
+      <p>
+        <label>Add Item</label>
+        <input
+          id="new-task"
+          type="text"
+          value={value}
+          name="todoField"
+          onKeyDown={handleKeyPress}
+          onChange={onValueChange}
+        />
+        <button onClick={addTodo}>Add</button>
+      </p>
+
+      <h3>Todo</h3>
+      <ul id="incomplete-tasks">
+        {todos
+          .filter(todo => !todo.status)
+          .map((todo, i) => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.status}
+                onChange={() => handleCheckboxChange(todo.id)}
+              />
+
+              <label>{todo.name}</label>
+              <button className="delete" onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </li>
+          ))}
+      </ul>
+
+      <h3>Completed</h3>
+      <ul id="completed-tasks">
+        {todos
+          .filter(todo => todo.status)
+          .map((todo, i) => (
+            <li key={i}>
+              <input type="checkbox" checked={todo.status} onChange={() => handleCheckboxChange(todo.id)} />
+              <label>{todo.name}</label>
+              <button className="delete" onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+
+}
+
+export default App;
